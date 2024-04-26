4c1f4ca262576e5487d6b91d0626c36c63eb18ca
Renato Sgarbossa Iesque <125359611+renatosgarbossa@users.noreply.github.com>
2024-04-26 08:37:47 -0300

    Create Formulario Cadastro estilo.js

---
+++ Formulario Cadastro estilo.js
@@ -0,0 +1,60 @@
+// estilo.js
+
+export const estilo = {
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center', 
+    justifyContent: 'center', 
+  },
+  image: {
+    alignSelf: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  logo: {
+    width: 270,
+    height: 140,
+  },
+  h1: {
+    fontSize: 40,
+    textAlign: 'center',
+  },
+  textoLabel: {
+    fontSize: 14,
+    color: '#000000',
+    marginLeft: 20,
+    marginBottom: 15,
+    marginTop: 15,
+  },
+  textoInput: {
+    color: '#ffffff',
+    borderWidth: 1,
+    borderColor: '#fff',
+    backgroundColor: '#000',
+    width: 300,
+    marginLeft: 20,
+    padding: 10,
+    borderRadius: 5,
+  },
+  link: {
+    color: '#000000',
+    fontSize: 14,
+    textAlign: 'center',
+    marginTop: 10,
+  },
+  buttonContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  continueButton: {
+    backgroundColor: '#007AFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  continueButtonText: {
+    color: '#ffffff',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+};
