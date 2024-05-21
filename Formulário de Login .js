7f2d2394f726f9a7e60cc7affbf8bbf66df87cba
Renato Sgarbossa Iesque <125359611+renatosgarbossa@users.noreply.github.com>
2024-05-21 10:17:19 -0300

    Atualizar o Formulário de Login .js

--- Formulário de Login .js
+++ Formulário de Login .js
@@ -43,3 +43,48 @@ const App = () => {
 
 export default App;
 
+*/style*/
+
+export const estilo = {
+  container:{
+    flex: 1,
+    backgroundColor: '#ffffff'
+  },
+  image:{
+    alignSelf:'center',
+    marginTop:20,
+    marginBottom: 20
+  },
+  logo: {
+    width: 270,
+    height: 140
+  },
+  h1: {
+    fontSize: 40,
+    textAlign: 'center'
+  },
+  textoLabel:{
+    fontSize: 14,
+    color: '#000000',
+    marginLeft: 20,
+    marginBottom: 15,
+    marginTop: 15
+  },
+  textoInput:{
+    color: '#ffffff',
+    borderWidth: 1,
+    borderColor: '#fff',
+    backgroundColor: '#000',
+    width: 300,
+    marginLeft: 20,
+    padding: 10,
+    borderRadius: 5
+  },
+  link: {
+    color: '#000000',
+    fontSize: 14,
+    textAlign: 'center',
+    marginTop: 10
+  }
+}
+
